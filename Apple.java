888d5c0a1d9a61f607a7f21cd418bc540f4aa865
IvanMak626 <341034262@gapps.yrdsb.ca>
2023-11-26 15:23:53 -0500

    apple increases falling speed every 5 apples

--- Apple.java
+++ Apple.java
@@ -12,9 +12,12 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int speed = 1;
     public void act()
     {
-        setLocation(getX(), getY()+2);
+        int x = getX();
+        int y = getY() + speed;        
+        setLocation(x, y);
         
         // remove apple and draw game over when apple gets to bottom
         MyWorld world = (MyWorld) getWorld();
@@ -22,5 +25,10 @@ public class Apple extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
--- TitleScreen.java
+++ TitleScreen.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elephant", 60);
+    Label titleLabel = new Label("Chompy elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,10 +17,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+
+        addObject(titleLabel, 220, 100);
+        prepare();
     }
-    
+
     // the main world act loop
     public void act()
     {
@@ -30,5 +31,19 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,520,105);
+        Label label = new Label("Use \u2190 and \u2192 to Move", 50);
+        addObject(label,getWidth()/2,220);
+        Label labelOne = new Label("Press <space> to Start", 50);
+        addObject(labelOne,getWidth()/2,300);
     }
 }
