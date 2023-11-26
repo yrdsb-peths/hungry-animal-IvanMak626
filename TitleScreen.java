286fb53bb4aad35ccab9be7f7af5856b6ced1b33
IvanMak626 <341034262@gapps.yrdsb.ca>
2023-11-26 14:58:41 -0500

    created title screen, when user presses space bar the game starts

---
+++ TitleScreen.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (Ivan Mak) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+    Label titleLabel = new Label("Hungry Elephant", 60);
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        
+        addObject(titleLabel, getWidth()/2, getHeight()/2);
+    }
+    
+    // the main world act loop
+    public void act()
+    {
+        // Start the game if user presses space bar
+        if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+}
